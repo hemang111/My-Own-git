e8f6c722a56b99369d6c320a2d0e1dd306de1ddd
Hemang Choudhary <hemangc37@gmail.com>
2024-12-26 18:33:45 +0530

    stage-0

--- app/git/commands/create-tree.js
+++ app/git/commands/create-tree.js
@@ -11,7 +11,7 @@ class create_tree_git{
   const directory = sha.slice(0, 2);
   const fileName = sha.slice(2);
   const filePath = path.join(process.cwd(), ".git", "objects", directory, fileName);
-  let inflatedContent = zlib.inflateSync(fs.readFileSync(filePath));
+  let inflatedContent = zlib.inflateSync(fs.readFileSync(filePath)).toString();
   let content = inflatedContent.slice(1).filter(value => value.includes(" "));
   let names = content.map(value => value.split(" ")[1]);
   names.forEach((name) => process.stdout.write(`${name}\n`));
