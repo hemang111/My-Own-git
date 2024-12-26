e6732f5dd79ff8a8adc6333c6095bb118fcd7ae4
Hemang Choudhary <hemangc37@gmail.com>
2024-12-27 02:16:43 +0530

    stage-0

--- app/git/commands/cloneRepo.js
+++ app/git/commands/cloneRepo.js
@@ -1,10 +1,21 @@
-class CloneRepo{
-    constructor(){
-
+class CloneRepo {
+    constructor(repositoryUrl, destinationPath) {
+      this.repositoryUrl = repositoryUrl;
+      this.destinationPath = destinationPath;
     }
-    run(){
-
+  
+    run() {
+      const { exec } = require('child_process');
+  
+      exec(`git clone ${this.repositoryUrl} ${this.destinationPath}`, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+      });
     }
-}
-
-module.exports = CloneRepo;
+  }
+  
+  module.exports = CloneRepo;
