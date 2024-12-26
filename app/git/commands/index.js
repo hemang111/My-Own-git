b7f6f4242457d9faeae0cd791625cae9e49bcdba
Hemang Choudhary <hemangc37@gmail.com>
2024-12-26 18:23:04 +0530

    stage-0

--- app/git/commands/index.js
+++ app/git/commands/index.js
@@ -1,4 +1,4 @@
 const CatFileCommand = require("./cat-file");
 const HashwriteCommand = require("./hash-write");
-const create_tree_git = require("./Create-ls-tree");
+const create_tree_git = require("./create-tree");
 module.exports = {CatFileCommand,HashwriteCommand,create_tree_git};
